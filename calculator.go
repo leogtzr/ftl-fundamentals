f5ba3cbdddaefdcbb184a746a6789cf80aee2882
leogtzr <leogutierrezramirez@gmail.com>
2020-09-06 21:53:28 -0600

    add Divide() function and it's test

--- calculator.go
+++ calculator.go
@@ -1,13 +1,31 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"fmt"
+)
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	return a - b
+}
+
+// Multiply takes two numbers and returns the result of multiplying the second
+// from the first.
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+// Divide ...
+func Divide(a, b float64) (float64, error) {
+	if b == 0.0 {
+		return 0.0, fmt.Errorf("cannot divide by 0")
+	}
+	return a / b, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -14,11 +14,59 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+
+	const want = 6.0
+	got := calculator.Multiply(2, 3)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		a, b        float64
+		want        float64
+		errExpected bool
+	}
+
+	tests := []testCase{
+		{
+			a:           6,
+			b:           0,
+			want:        0,
+			errExpected: true,
+		},
+		{
+			a:           3,
+			b:           2,
+			want:        1.5,
+			errExpected: false,
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if tc.errExpected {
+			if err == nil {
+				t.Error("expecting an error")
+			}
+		}
+		if got != tc.want {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+
+}
